c48582a5ed330fd68e552e5dbb236f5208db95ad
yuripbong <kyri200350@gmail.com>
2025-01-11 15:35:19 +0900

    [test] Custom User 생성

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "temp",
     'drf_yasg',
-
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -146,3 +146,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
