fba7caa3f98c586a9c447acf432a42e0f708e0dd
yuripbong <kyri200350@gmail.com>
2025-01-23 20:43:09 +0900

    [style] 불필요 urls 주석처리

--- temp/pdf/urls.py
+++ temp/pdf/urls.py
@@ -3,7 +3,7 @@ from .views import PDFUploadView, PDFPageTextView, PDFDeleteByFileIDView, PDFGen
 urlpatterns = [
     path('upload/', PDFUploadView.as_view(), name='file-upload'),
     path('genealogy-upload/', GenealogyUploadView.as_view(), name='file-upload'),
-    path('<int:file_id>/page/<int:page_number>/', PDFPageTextView.as_view(), name='page-text'),
+    #path('<int:file_id>/page/<int:page_number>/', PDFPageTextView.as_view(), name='page-text'),
     path('delete/<int:file_id>/', PDFDeleteByFileIDView.as_view(), name='delete_file_data'),
-    path('summary-pdf/<str:redis_key>/', PDFGenerateView.as_view(), name='summary_pdf'),
+    #path('summary-pdf/<str:redis_key>/', PDFGenerateView.as_view(), name='summary_pdf'),
 ]
--- temp/urls.py
+++ temp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProcessRedisDataView
 
 urlpatterns = [
-    path('process/', ProcessRedisDataView.as_view(), name='process-redis-data'),
+    #path('process/', ProcessRedisDataView.as_view(), name='process-redis-data'),
 ]
