ccf1a01c22d4aa7be0a7622cd0a2bf35b8a18886
JJeonJong <jeonjong0915@gmail.com>
2025-01-24 23:12:51 +0900

    feat : 헷갈린 문제 출력

--- temp/question/urls.py
+++ temp/question/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import TopicsAndQuestionsRAGView, SubmitAnswerAPIView, DeleteQuestionView, DeleteUserAnswerView, WrongAnswerView, AllQuestionsView
+from .views import (TopicsAndQuestionsRAGView, SubmitAnswerAPIView, DeleteQuestionView,
+                    DeleteUserAnswerView, WrongAnswerView, AllQuestionsView, ConfusedAnswerView)
 
 urlpatterns = [
     path('create/', TopicsAndQuestionsRAGView.as_view(), name='create'),
@@ -8,5 +9,6 @@ urlpatterns = [
     path("answers/<int:answer_id>/delete/", DeleteUserAnswerView.as_view(), name="delete-user-answer"),
     path('incorrect-answers/', WrongAnswerView.as_view(), name='incorrect_answers_api'),
     path('all-questions/', AllQuestionsView.as_view(), name='all_questions_api'),
+    path('confused-answers/', ConfusedAnswerView.as_view(), name='confused_answers_api'),
 
 ]
