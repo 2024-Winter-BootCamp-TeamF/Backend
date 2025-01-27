4d3484dabf7283f21710f4ca2e88ade28e454c30
kim jongyeon <qsc6543@inha.edu>
2025-01-27 17:18:22 +0900

    refactor) /빼기

--- temp/question/urls.py
+++ temp/question/urls.py
@@ -3,12 +3,12 @@ from .views import (TopicsAndQuestionsRAGView, SubmitAnswerAPIView, DeleteQuesti
                     DeleteUserAnswerView, WrongAnswerView, AllQuestionsView, ConfusedAnswerView)
 
 urlpatterns = [
-    path('create/', TopicsAndQuestionsRAGView.as_view(), name='create'),
-    path('submit-answer/', SubmitAnswerAPIView.as_view(), name='submit-answer'),
-    path("questions/<int:question_id>/delete/", DeleteQuestionView.as_view(), name="delete-question"),
-    path("answers/<int:answer_id>/delete/", DeleteUserAnswerView.as_view(), name="delete-user-answer"),
-    path('incorrect-answers/', WrongAnswerView.as_view(), name='incorrect_answers_api'),
-    path('all-questions/', AllQuestionsView.as_view(), name='all_questions_api'),
-    path('confused-answers/', ConfusedAnswerView.as_view(), name='confused_answers_api'),
+    path('create', TopicsAndQuestionsRAGView.as_view(), name='create'),
+    path('submit-answer', SubmitAnswerAPIView.as_view(), name='submit-answer'),
+    path("questions/<int:question_id>/delete", DeleteQuestionView.as_view(), name="delete-question"),
+    path("answers/<int:answer_id>/delete", DeleteUserAnswerView.as_view(), name="delete-user-answer"),
+    path('incorrect-answers', WrongAnswerView.as_view(), name='incorrect_answers_api'),
+    path('all-questions', AllQuestionsView.as_view(), name='all_questions_api'),
+    path('confused-answers', ConfusedAnswerView.as_view(), name='confused_answers_api'),
 
 ]
